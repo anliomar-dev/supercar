7f6e7f042caa0b91c9db716cc5b9805d65e5e092
Anli omar <omaranli56@gmail.com>
2024-12-02 15:50:48 +0400

    create and configure conenction to database

--- app/database/Database.php
+++ app/database/Database.php
@@ -2,7 +2,25 @@
 require_once(__DIR__ . '/../../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
-class Database
-{
+class Database{
+    private $conn;
+    public function __construct(){
+        // Récupérer les paramètres depuis le fichier .env
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'];
+
+        try{
+            $this->conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,5 @@
 <?php
+require_once(__DIR__ . '/../app/database/Database.php');
+
+$db = new Database();
+$conn = $db->getConnection();

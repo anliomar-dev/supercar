a335df57eef820f285a03482ce0ff7c305695d51
Anli omar <omaranli56@gmail.com>
2024-12-19 19:21:06 +0400

    added 2 themes in for daisyui in tailwind.config

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" data-theme="bumblebee">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -9,6 +9,9 @@
     <title>Accueil</title>
 </head>
 <body>
-    <h1 class="font-bold text-4xl">bonsoir bienvenue sur la page d'accueil</h1>
+    <h1 class="font-bold text-4xl text-amber-800">bonsoir bienvenue sur la page d'accueil</h1>
+    <button class="btn btn-primary">primary</button>
+    <button class="btn btn-secondary">secondary</button>
+    <p class="text-primary font-bold text-4xl">ceci est un paragraphe</p>
 </body>
 </html>

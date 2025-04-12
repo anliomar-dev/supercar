de53b4a9f749571c31f6b5fde50863396060bd56
Omar.py <omaranli56@gmail.com>
2025-04-12 18:01:13 +0400

    feat: displayed message when there is non event

--- views/evennement.php
+++ views/evennement.php
@@ -11,7 +11,7 @@
 	<body>
 		<main class="flex justify-center flex-wrap gap-5 py-14 px-8">
 			<!--<?php if(isset($events)){var_dump($events);};?>-->
-			<?php if(isset($events)): ?>
+			<?php if(isset($events) && !empty($events)): ?>
 				<?php foreach($events as $event):?>
 					<div class="card bg-base-200 w-96 shadow-sm">
 						<figure>
@@ -23,68 +23,17 @@
 							<h2 class="card-title"><?php echo $event["titre"];?></h2>
 							<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
 							<div class="card-actions justify-end">
-								<button class="btn btn-primary">Buy Now</button>
+								<a href="" class="btn btn-primary">Plus de détails</a>
 							</div>
 						</div>
 					</div>
-					<div class="card bg-base-200 w-96 shadow-sm">
-						<figure>
-							<img
-								src=<?php echo $event["image"];?>
-								alt=<?php echo $event["titre"]." image";?> />
-						</figure>
-						<div class="card-body">
-							<h2 class="card-title"><?php echo $event["titre"];?></h2>
-							<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-							<div class="card-actions justify-end">
-								<button class="btn btn-primary">Buy Now</button>
-							</div>
-						</div>
-					</div>
-					<div class="card bg-base-200 w-96 shadow-sm">
-						<figure>
-							<img
-								src=<?php echo $event["image"];?>
-								alt=<?php echo $event["titre"]." image";?> />
-						</figure>
-						<div class="card-body">
-							<h2 class="card-title"><?php echo $event["titre"];?></h2>
-							<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-							<div class="card-actions justify-end">
-								<button class="btn btn-primary">Buy Now</button>
-							</div>
-						</div>
-					</div>
-					<div class="card bg-base-200 w-96 shadow-sm">
-						<figure>
-							<img
-								src=<?php echo $event["image"];?>
-								alt=<?php echo $event["titre"]." image";?> />
-						</figure>
-						<div class="card-body">
-							<h2 class="card-title"><?php echo $event["titre"];?></h2>
-							<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-							<div class="card-actions justify-end">
-								<button class="btn btn-primary">Buy Now</button>
-							</div>
-						</div>
-					</div>
-					<div class="card bg-base-200 w-96 shadow-sm">
-						<figure>
-							<img
-								src=<?php echo $event["image"];?>
-								alt=<?php echo $event["titre"]." image";?> />
-						</figure>
-						<div class="card-body">
-							<h2 class="card-title"><?php echo $event["titre"];?></h2>
-							<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-							<div class="card-actions justify-end">
-								<button class="btn btn-primary">Buy Now</button>
-							</div>
-						</div>
-					</div>
-                <?php endforeach; ?>
-			<?php endif; ;?>
+					
+        <?php endforeach; ?>
+			<?php else: ?>
+				<div class="w-full flex justify-center items-center pb-52">
+					<h1 class="text-primary text-2xl">Aucun événement pour le moment</h1>
+				</div>	
+			<?php endif; ?>
 		</main>
 		<script type="module" src="/supercar/assets/js/navbar.js" defer></script>
 	</body>
